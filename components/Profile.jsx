2c6943d844dda773961999f8b0209a774e6c78a6
Dart95 <danieikia@mail.ru>
2021-12-10 15:18:10 +0300

    modificate Profile and SVG

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
-// import { ReactSVG } from "react-svg";
-// import SVGicon from "./SVG";
-// import Reactsvg from "./Img/react-js";
-
+import SVGicon from "../components/SVG";
 export default function ButtonLink() {
   return (
     <Typography>
-      <Link
-        component="button"
-        variant="body2"
-        onClick={() => {
-          console.info("I'm a button.");
-        }}
-      >
-        Button Link
-      </Link>
-
-      <img src="../components/Img/react-js.svg"></img>
+      <SVGicon />
     </Typography>
   );
 }
--- components/SVG.jsx
+++ components/SVG.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 const useStyles = makeStyles((theme) => ({
+  // "@keyframes fadeAnimation": {
+  //   from: { transform: "0deg" },
+  //   to: { transform: "180deg" },
+  // },
   Logo: {
-    height: "60px",
+    height: "30vmin",
     pointerEvents: "none",
+    // backgroundColor: "grey",
+    marginTop: "20px",
+    animation: "infinite 10s linear ",
   },
 }));
+
 export default function ButtonLink() {
   const classes = useStyles();
   return (
-    <img className={classes.Logo} src="../components/Img/react-js.svg"></img>
+    <center>
+      <img className={classes.Logo} src="../components/Img/react-js.svg"></img>
+    </center>
   );
 }
